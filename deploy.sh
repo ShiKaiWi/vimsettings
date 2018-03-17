79dd6c10657df715dbbd73a2dfed1ee77d135ecb
ShiKaiWi <xykwei@gmail.com>
2018-03-18 01:09:06 +0800

    separate the vim deployment which is time cost

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,8 @@
 git submodule init
 git submodule update
 
-# deploy the .vim and .vimrc to $HOME
-rm -rf ~/.vimrc ~/.vim
-cp -r .vimrc .vim ~/
-
 # deploy the bash rc files
-cp ./bash_utils/bashrcs/* ~/
+cp -R ./bash_utils/bashrcs/ ~/
 
 # deploy the z command
 if [ ! -d ~/.zc ];then
@@ -21,7 +17,7 @@ fi
 if [ ! -d ~/bin ]; then
     mkdir ~/bin
 fi
-cp ~/.bash_utils/custom_commands/* ~/bin
+cp -R ./bash_utils/custom_commands/ ~/bin
 
 
 # clear all the submodules
---
+++ deploy_vim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+vundle_module=vim/.vim/bundle/Vundle.vim
+# install all the submodules
+git submodule init $vundle_module
+git submodule update $vundle_module
+
+# deploy the .vim and .vimrc to $HOME
+rm -rf ~/.vimrc ~/.vim
+cp -r .vimrc .vim ~/
+vim +PluginInstall +qall
+
+git submodule deinit $vundle_module
