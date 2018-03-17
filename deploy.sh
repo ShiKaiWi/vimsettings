2b77b4a9ac31dc695aa823b2b4cee15f096fb42f
ShiKaiWi <xykwei@gmail.com>
2018-03-18 01:21:06 +0800

    fix bugs about the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# install all the submodules
-git submodule init
-git submodule update
+echo "install all the submodules..."
+git submodule init > /dev/null
+git submodule update > /dev/null
+echo "done!"
 
-# deploy the bash rc files
+echo "deploy the bash rc files..."
 cp -R ./bash_utils/bashrcs/ ~/
+echo "done!"
 
-# deploy the z command
+echo "deploy the z command..."
 if [ ! -d ~/.zc ];then
     mkdir ~/.zc
     cp -r ./bash_utils/z ~/.zc
 fi
+echo "done!"
 
-# deploy the custom commands
+echo "deploy the custom commands..."
 if [ ! -d ~/bin ]; then
     mkdir ~/bin
 fi
 cp -R ./bash_utils/custom_commands/ ~/bin
+echo "done!"
 
-# deploy the vscode settings
-cp -R ./vscode/ ~/Library/Application Support/Code/User/
+echo "deploy the vscode settings..."
+cp -R ./vscode/ ~/Library/Application\ Support/Code/User/
+echo "done!"
 
-# clear all the submodules
-git submodule deinit --all
+echo "clear all the submodules..."
+git submodule deinit --all > /dev/null
+echo "done!"
