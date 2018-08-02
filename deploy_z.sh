1e14069de7b4cb71030716c82cf9d2d8a60667e5
xwei <xykwei@gmail.com>
2018-08-02 09:22:22 +0800

    fix z deploy script

--- deploy_z.sh
+++ deploy_z.sh
@@ -3,4 +3,4 @@ if [ ! -d ~/.zc ];then
     mkdir ~/.zc
     cp -r ./bash_utils/z ~/.zc
 fi
-echo "it will be done if you append the line: 'source ~/.zc' to your bash config"
+echo "it will be done if you append the line: 'source ~/.zc/z/z.sh' to your bash config"
