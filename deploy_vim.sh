6b308a83092f4169e3a220bb3a378ac97c8f51f0
xwei <xykwei@gmail.com>
2018-08-08 17:27:01 +0800

    fix missing .git dir in Vundle.vim submodule

--- deploy_vim.sh
+++ deploy_vim.sh
@@ -4,6 +4,9 @@ vundle_module=vim/.vim/bundle/Vundle.vim
 # install vundle submodule
 git submodule init $vundle_module
 git submodule update $vundle_module
+rm -rf $vundle_module/.git
+cp -r .git/modules/.vim/bundle/Vundle.vim $vundle_module/.git
+rm -rf $vundle_module/.git/config
 
 # deploy the .vim and .vimrc to $HOME
 if [ ! -d ~/.vim ];then
